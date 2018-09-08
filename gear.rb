275a1f608d890b99e0566e21decf46ad4a388ddb
Henry Cooksley <henry.cooksley@gmail.com>
2018-09-08 13:50:48 +0100

    gear 2.19

--- gear.rb
+++ gear.rb
@@ -1,9 +1,9 @@
 class Gear
   attr_reader :chainring, :cog, :wheel
-  def initialize(chainring, cog, rim, tire)
+  def initialize(chainring, cog, wheel=nil)
     @chainring = chainring
     @cog = cog
-    @wheel = Wheel.new(rim, tire)
+    @wheel = wheel
   end
 
   def ratio
@@ -13,10 +13,28 @@ class Gear
   def gear_inches
     ratio * wheel.diameter
   end
+end
 
-  Wheel = Struct.new(:rim, :tire) do
-    def diameter
-      rim + (tire * 2)
-    end
+class Wheel
+  attr_reader :rim, :tire
+
+  def initialize(rim, tire)
+    @rim = rim
+    @tire = tire
+  end
+
+  def diameter
+    rim + (tire * 2)
+  end
+
+  def circumference
+    diameter * Math::PI
   end
 end
+
+@wheel = Wheel.new(26, 1.5)
+puts @wheel.circumference
+
+puts Gear.new(52, 11, @wheel).gear_inches
+
+puts Gear.new(52, 11).ratio

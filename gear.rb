09201a2395ec15f4f92b14d1ec6eb61efbeb43cc
Henry Cooksley <henry.cooksley@gmail.com>
2018-09-10 11:27:14 +0100

    gear 3.5

--- gear.rb
+++ gear.rb
@@ -1,9 +1,10 @@
 class Gear
-  attr_reader :chainring, :cog, :wheel
+  attr_reader :chainring, :cog, :rim, :tire
   def initialize(chainring, cog, rim, tire)
     @chainring = chainring
     @cog = cog
-    @wheel = Wheel.new(rim, tire)
+    @rim = rim
+    @tire = tire
   end
 
   def gear_inches
@@ -12,6 +13,10 @@ class Gear
 
   def ratio
     chainring / cog.to_f
+  end
+
+  def wheel
+    @wheel ||= Wheel.new(rim, tire)
   end
 # ...
 end

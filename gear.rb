6aca42bf8350794b65cee033260bcdbd16eda6de
Henry Cooksley <henry.cooksley@gmail.com>
2018-09-10 12:22:04 +0100

    gear 3.15

--- gear.rb
+++ gear.rb
@@ -1,24 +1,33 @@
-class Gear
-  attr_reader :chainring, :cog, :wheel
-  def initialize(chainring: default_chainring, cog: 18, wheel:)
-    @chainring = chainring
-    @cog = cog
-    @wheel = wheel
-  end
+module SomeFramework
+  class Gear
+    attr_reader :chainring, :cog, :wheel
+    def initialize(chainring, cog, wheel)
+      @chainring = chainring
+      @cog = cog
+      @wheel = wheel
+    end
 
-  def default_chainring
-    (100/2) - 10 # silly code, useful example
-  end
+    def default_chainring
+      (100/2) - 10 # silly code, useful example
+    end
 
-  def gear_inches
-    ratio * wheel.diameter
-  end
+    def gear_inches
+      ratio * wheel.diameter
+    end
 
-  def ratio
-    chainring / cog.to_f
-  end
+    def ratio
+      chainring / cog.to_f
+    end
 
-# ...
+  # ...
+  end
+end
+
+# wrap the interface to protect yourself from changes
+module GearWrapper
+  def self.gear(chainring:, cog:, wheel:)
+    SomeFramework::Gear.new(chainring, cog, wheel)
+  end
 end
 
 class Wheel
@@ -34,6 +43,7 @@ class Wheel
 # ...
 end
 
-puts Gear.new(wheel: Wheel.new(26, 1.5)).chainring
-
-puts Gear.new(chainring: 52, wheel: Wheel.new(26, 1.5)).chainring
+puts GearWrapper.gear(
+  chainring: 52,
+  cog: 11,
+  wheel: Wheel.new(26, 1.5)).gear_inches

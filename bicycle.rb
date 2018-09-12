c29d6126da0a8fc375c6bf8e44a15731bc18fc9a
Henry Cooksley <henry.cooksley@gmail.com>
2018-09-12 15:17:45 +0100

    bicycle 6.2

--- bicycle.rb
+++ bicycle.rb
@@ -1,28 +1,45 @@
 class Bicycle
-  attr_reader :size, :tape_color
+  attr_reader :style, :size,
+  :tape_color,
+  :front_shock, :rear_shock
 
   def initialize(**opts)
+    @style = opts[:style]
     @size = opts[:size]
     @tape_color = opts[:tape_color]
+    @front_shock = opts[:front_shock]
+    @rear_shock = opts[:rear_shock]
   end
 
-  # every bike has the same defaults for
-  # tire and chain size
+  # checking 'style' starts down a slippery slope
   def spares
-    { chain: '11-speed',
-      tire_size: '23',
-      tape_color: tape_color }
+    if style == :road
+      { chain: '11-speed',
+        tire_size: '23', # millimeters
+        tape_color: tape_color }
+    else
+      { chain: '11-speed',
+      tire_size: '2.1', # inches
+      front_shock: front_shock }
+    end
   end
-
-  # Many other methods...
+  # ...
 end
 
+
 bike = Bicycle.new(
+  style: :mountain,
+  size: 'S',
+  front_shock: 'Manitou',
+  rear_shock: 'Fox')
+
+puts bike.spares
+# => {:chain=>"11-speed", :tire_size=>"2.1", :front_shock=>"Manitou"}
+
+bike = Bicycle.new(
+  style: :road,
   size: 'M',
   tape_color: 'red')
-
-puts bike.size
-# => M
 
 puts bike.spares
 # => {:chain=>"11-speed", :tire_size=>"23", :tape_color=>"red"}

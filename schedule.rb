715740cb30beaef82d948578deb4b6e70ef894a2
Henry Cooksley <henry.cooksley@gmail.com>
2018-09-13 12:29:38 +0100

    schedule 7.6

--- schedule.rb
+++ schedule.rb
@@ -42,3 +42,27 @@ ending = Date.parse("2019/09/10")
 
 b = Bicycle.new
 puts b.schedulable?(starting, ending)
+
+class Vehicle
+  include Schedulable
+
+  def lead_days
+    3
+  end
+  # ...
+end
+
+class Mechanic
+  include Schedulable
+
+  def lead_days
+    4
+  end
+  # ...
+end
+
+v = Vehicle.new
+puts v.schedulable?(starting, ending)
+
+m = Mechanic.new
+puts m.schedulable?(starting, ending)

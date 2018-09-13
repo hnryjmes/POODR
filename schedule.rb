a4879a5b8145a2c67198451dfe85afa4463c4680
Henry Cooksley <henry.cooksley@gmail.com>
2018-09-13 12:26:32 +0100

    schedule 7.5

--- schedule.rb
+++ schedule.rb
@@ -6,28 +6,30 @@ class Schedule
   end
 end
 
-class Bicycle
-  attr_reader :schedule, :size, :chain, :tire_size
+module Schedulable
+  attr_writer :schedule
 
-  # Inject the Schedule and provide a default
-  def initialize(**opts)
-    @schedule = opts[:schedule] || Schedule.new
-    # ...
+  def schedule
+    @schedule ||= Schedule.new
   end
 
-  # Return true if this bicycle is available
-  # during this (now Bicycle specific) interval.
   def schedulable?(starting, ending)
     !scheduled?(starting - lead_days, ending)
   end
 
-  # Return the schedule's answer
   def scheduled?(starting, ending)
     schedule.scheduled?(self, starting, ending)
   end
 
-  # Return the number of lead_days before a bicycle
-  # can be scheduled.
+  # includers may override
+  def lead_days
+    0
+  end
+end
+
+class Bicycle
+  include Schedulable
+
   def lead_days
     1
   end
